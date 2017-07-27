12a679f0a73ded8610d0ecd6660d0db012c59789
Victor Costa <vfcosta@gmail.com>
2017-07-27 19:06:01 -0300

    Refactor trajectory generation using JMT

--- src/trajectory_generation.hpp
+++ src/trajectory_generation.hpp
@@ -9,6 +9,7 @@ public:
   TrajectoryGeneration(vector<double> map_waypoints_x, vector<double> map_waypoints_y, vector<double> map_waypoints_s, vector<double> map_waypoints_dx, vector<double> map_waypoints_dy);
   vector<vector<double>> generate(double car_x, double car_y, double car_s, double car_d, double car_yaw, double car_speed, vector<double> previous_path_x, vector<double> previous_path_y);
   vector<double> JMT(vector<double> start, vector<double> end, double time);
+  void jmtTrajectory(vector<double> start, vector<double> end, double time, int lane, vector<double> &next_x_vals, vector<double> &next_y_vals);
 
 private:
   vector<double> map_waypoints_x;
